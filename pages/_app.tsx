8fdd54a02901198725e92d84d687f139b3055978
Joshua Wootonn <joshuawootonn@gmail.com>
2023-10-10 16:27:59 -0500

    feat: maintainence work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from "next/app";
 import "../style/global.css";
 
+import localFont from "next/font/local";
+
+const myFont = localFont({
+  src: "./Aloevera.ttf",
+  variable: "--font-aloevera",
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // <main className={myFont.className}>
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/api/makeswift/[...makeswift].ts
+++ pages/api/makeswift/[...makeswift].ts
@@ -1,5 +1,17 @@
 import { MakeswiftApiHandler } from "@makeswift/runtime/next";
 
 export default MakeswiftApiHandler(process.env.MAKESWIFT_SITE_API_KEY!, {
+  getFonts: () => [
+    {
+      family: "var(--font-aloevera)",
+      variants: [
+        {
+          weight: "400",
+          style: "normal",
+          src: "/Aloevera.otf",
+        },
+      ],
+    },
+  ],
   appOrigin: process.env.MAKESWIFT_APP_ORIGIN,
 });
